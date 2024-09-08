180a408dffd919299a733e3d8db1d89fd75606b6
Sahibdeep Singh <sahibdeepsinghj@gmail.com>
2024-09-08 07:35:30 +0530

    staticfiles

--- backstage/settings.py
+++ backstage/settings.py
@@ -127,7 +127,10 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # For media files (like the image you're referencing)
 MEDIA_URL = '/media/'
